3edd5b184e9b3befee8638abfca472775b031cca
Raven A. Tongol <raven.tongol@cit.edu>
2026-09-24 01:37:11 +0800

    1.2: course information step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 const App = () => {
   const course = 'Data Analytics 1'
-  const part = 'Apps Development & Emerging Tech'
-  const units = 3
+
+  const part1 = 'Apps Development & Emerging Tech'
+  const units1 = 3
+
+  const part2 = 'RIZAL031 - The Life and Works of Rizal'
+  const units2 = 3
+
+  const part3 = 'Technopreneurship'
+  const units3 = 3
+
+  const total = units1 + units2 + units3
 
   return (
     <div>
       <h1>{course}</h1>
       <p>
-        {part} {units}
+        {part1} {units1}
+      </p>
+      <p>
+        {part2} {units2}
+      </p>
+      <p>
+        {part3} {units3}
+      </p>
+      <p>
+        <strong>Total units {total}</strong>
       </p>
     </div>
   )
